b9586f02c595fe425ce841349deda3a84a227fb5
Olena-321 <lena37376@gmail.com>
2025-08-02 19:59:44 +0300

    Add file_1.js with homework 5.1 'Написати функцію pow'

---
+++ file_1.js
@@ -0,0 +1,34 @@
+//Варіант 1
+
+function pow(x,y) {
+    let result = x 
+for (let i=1; i<y; i++) {
+result = result * x /* Tекущее значение переменной result и умножь на x,
+затем положить обратно это новое значение в result*/
+}
+return result
+}
+console.log(pow(2,3))
+
+
+/*Варіант 2
+
+let pow=function (x,y) {
+    let result = x 
+for (let i=1; i<y; i++) {
+result = result*x
+}
+return result
+}
+console.log(pow(2, 3))
+
+Варіант 3
+
+function pow(x,y) {
+    let result = 1
+    for (let i=0; i<y; i++){
+        result= result * x
+}
+return result
+}
+console.log(pow(2,3))*/
