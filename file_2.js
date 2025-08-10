ec3cd0a925f3a1eabb2ef50613d2f5f4abad7728
Olena-321 <lena37376@gmail.com>
2025-08-10 18:45:16 +0300

    Add changes to homework 6

--- file_2.js
+++ file_2.js
@@ -30,3 +30,36 @@ function checkProbabilityTheory(count) {
   
   // виклик функції :
   checkProbabilityTheory(500);
+
+  function checkProbabilityTheory(count) {
+    // Лічильники оголошуємо ПОЗА циклом,
+    // щоб вони акумулювали значення по всьому циклу
+    let evenNumbers = 0; // Кількість парних чисел
+    let oddNumbers = 0;  // Кількість непарних чисел
+  
+    for (let i = 0; i < count; i++) {
+      // Кожного разу генеруємо нове випадкове число всередині циклу
+      let randomNumber = Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
+  
+      // Перевіряємо парність і збільшуємо відповідний лічильник зміна в циклі
+      if (randomNumber % 2 === 0) {
+        evenNumbers++;
+      } else {
+        oddNumbers++;
+      }
+    }
+  
+    // Обчислюємо відсотки парних і непарних чисел
+    let evenPercent = (evenNumbers / count) * 100;
+    let oddPercent = (oddNumbers / count) * 100;
+  
+    // Виводимо результати в консоль
+    console.log("Кількість згенерованих чисел:", count);
+    console.log("Парних чисел:", evenNumbers);
+    console.log("Непарних чисел:", oddNumbers);
+    console.log("Відсоток парних:", evenPercent.toFixed(2) + "%");
+    console.log("Відсоток непарних:", oddPercent.toFixed(2) + "%");
+  }
+  
+  // Виклик функції з аргументом 500
+  checkProbabilityTheory(500);
